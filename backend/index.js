297dc8c31ace7e91049b492ddf2c5981804e7f0c
Vinay Salve <vinaysalve123@gmail.com>
2025-06-19 18:04:20 +0530

    mongoose url fixed

--- backend/index.js
+++ backend/index.js
@@ -4,7 +4,7 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 require("dotenv").config();
 const PORT = process.env.PORT || 5001;
-const MONGO = process.env.MANGO;
+const MONGO = process.env.MONGO;
 const actorRoutes = require("./routes/actor");
 
 //Used to parse data to json
